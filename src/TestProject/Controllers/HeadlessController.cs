6c21e514b35398f4c2608b664479771f46cd8385
Nikcio <nikolajgive@gmail.com>
2021-09-25 14:14:16 +0200

    Updated umbraco

--- src/TestProject/Controllers/HeadlessController.cs
+++ src/TestProject/Controllers/HeadlessController.cs
@@ -1,5 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Nikcio.Umbraco.Headless.Core.Commands.Sites.Pages.PageData;
+using Nikcio.Umbraco.Headless.Core.Mappers.Sites.Pages.PageData;
+using Nikcio.Umbraco.Headless.Core.Models.SiteModels.PageModels.PropertyModels;
 using Nikcio.Umbraco.Headless.Core.Services.Headless;
+using System;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
 using Umbraco.Cms.Web.Common.Attributes;
 using Umbraco.Cms.Web.Common.Controllers;
 
@@ -18,6 +24,24 @@ namespace TestProject.Controllers
         public virtual IActionResult GetData(string route)
         {
             return new OkObjectResult(headlessService.GetData(route));
+        }
+    }
+
+    public class TestComposer : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            PropertyMapper.AddMapping(Umbraco.Cms.Core.Constants.PropertyEditors.Aliases.MediaPicker3, x => new NewTestClass(x));
+        }
+    }
+
+    public class NewTestClass : PropertyModel
+    {
+        public string CustomValue { get; set; }
+
+        public NewTestClass(ICreatePropertyCommandBase createPropertyCommandBase) : base(createPropertyCommandBase)
+        {
+            CustomValue = DateTime.Now.ToLongDateString();
         }
     }
 }
