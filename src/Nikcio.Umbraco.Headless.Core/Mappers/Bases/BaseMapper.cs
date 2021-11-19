eae31361af13b4323f9041c5b6ddda3bceb1be79
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2021-11-19 17:13:57 +0100

    pr-test

--- src/Nikcio.Umbraco.Headless.Core/Mappers/Bases/BaseMapper.cs
+++ src/Nikcio.Umbraco.Headless.Core/Mappers/Bases/BaseMapper.cs
@@ -18,5 +18,19 @@ namespace Nikcio.Umbraco.Headless.Core.Mappers.Bases
                 }
             }
         }
+
+        public void test<TType>(string key, Dictionary<string, string> map) where TType : class {
+            key = key.ToLowerInvariant();
+            if (!map.ContainsKey(key))
+            {
+                lock (map)
+                {
+                    if (!map.ContainsKey(key))
+                    {
+                        map.Add(key, typeof(TType).AssemblyQualifiedName);
+                    }
+                }
+            }
+        }
     }
 }
