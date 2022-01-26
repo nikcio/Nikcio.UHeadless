262c91ab0c7b4322cba1df193b46cfc0e52bc672
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2022-01-26 15:12:46 +0100

    feat: Partial reflector factory

---
+++ src/Nikcio.UHeadless/Factories/Reflection/DependencyReflectorFactory.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nikcio.UHeadless.Factories.Reflection
+{
+    public class DependencyReflectorFactory
+    {
+        private readonly IServiceProvider serviceProvider;
+        private readonly ILogger<DependencyReflectorFactory> logger;
+
+        public DependencyReflectorFactory(IServiceProvider serviceProvider, ILogger<DependencyReflectorFactory> logger)
+        {
+            this.serviceProvider = serviceProvider;
+            this.logger = logger;
+        }
+
+        public T GetReflectedType<T>(Type typeToReflect, object[] constructorRequiredParamerters) 
+            where T : class
+        {
+            var propertyTypeAssemblyQualifiedName = typeToReflect.AssemblyQualifiedName;
+            var constructors = typeToReflect.GetConstructors();
+            if(constructors.Length == 0)
+            {
+                LogConstructorError(typeToReflect, constructorRequiredParamerters);
+                return null;
+            }
+            var parameters = GetConstructor(constructors, constructorRequiredParamerters).GetParameters();
+            var injectedParamerters = constructorRequiredParamerters
+                .Concat(parameters.Skip(constructorRequiredParamerters.Length).Select(parameter => serviceProvider.GetService(parameter.ParameterType)))
+                .ToArray();
+            return (T)Activator.CreateInstance(Type.GetType(propertyTypeAssemblyQualifiedName), injectedParamerters);
+        }
+
+        private void LogConstructorError(Type typeToReflect, object[] constructorRequiredParamerters)
+        {
+            string constructorNames = string.Join(", ", constructorRequiredParamerters.Select(item => item.GetType().Name));
+            string message = $"Unable to create instance of {typeToReflect.Name}. " +
+                $"Could not find a constructor with {constructorNames} as first argument(s)";
+            logger.LogError(message);
+        }
+
+        private ParameterInfo[] TakeConstructorRequiredParamters(ConstructorInfo constructor, int constructorRequiredParamertersLength)
+        {
+            return constructor.GetParameters()?.Take(constructorRequiredParamertersLength).ToArray();
+        }
+
+        private bool ValidateConstructorRequiredParameters(ConstructorInfo constructor, object[] constructorRequiredParameters)
+        {
+            var parameters = TakeConstructorRequiredParamters(constructor, constructorRequiredParameters.Length);
+            for(int i = 0; i < parameters.Length; i++)
+            {
+                var requiredParameter = constructorRequiredParameters[i].GetType();
+                if(parameters[i].ParameterType != requiredParameter)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private ConstructorInfo GetConstructor (ConstructorInfo[] constructors, object[] constructorRequiredParameters)
+        {
+            return constructors?.FirstOrDefault(constructor =>
+              ValidateConstructorRequiredParameters(constructor, constructorRequiredParameters));
+        }
+    }
+}
--- src/Nikcio.UHeadless/Models/Properties/BlockList/BlockListModelGraphType.cs
+++ src/Nikcio.UHeadless/Models/Properties/BlockList/BlockListModelGraphType.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
 using Nikcio.UHeadless.Commands.Properties;
 using Nikcio.UHeadless.Factories.Properties;
 using Nikcio.UHeadless.Models.Dtos.Propreties.PropertyValues;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Umbraco.Cms.Core.Models.Blocks;
@@ -17,6 +19,26 @@ namespace Nikcio.UHeadless.Models.Properties.BlockList
             var value = (BlockListModel)createPropertyValue.Property.GetValue();
             Blocks = value.ToList()
                 ?.Select(blockListItem => new BlockListItemGraphType(blockListItem, mapper, propertyFactory, createPropertyValue.Content, createPropertyValue.Culture)).ToList();
+        }
+    }
+
+    public class BlockListModelGraphType<T> : PropertyValueBaseGraphType
+        where T : PropertyValueBaseGraphType
+    {
+        public List<T> Blocks { get; set; }
+
+        public BlockListModelGraphType(CreatePropertyValue createPropertyValue, IServiceProvider serviceProvider) : base(createPropertyValue)
+        {
+            var value = (BlockListModel)createPropertyValue.Property.GetValue();
+            Blocks = value.ToList()
+                ?.Select(blockListItem => {
+                    var propertyTypeAssemblyQualifiedName = blockListItem.GetType().AssemblyQualifiedName;
+                    var type = Type.GetType(propertyTypeAssemblyQualifiedName);
+                    var constructors = type.GetConstructors();
+                    var parameters = constructors.FirstOrDefault(constructor => constructor.GetParameters().FirstOrDefault().ParameterType == typeof(CreatePropertyValue)).GetParameters();
+                    var injectedParamerters = new object[] { createPropertyValue }.Concat(parameters.Skip(1).Select(parameter => serviceProvider.GetService(parameter.ParameterType))).ToArray();
+                    return (T)Activator.CreateInstance(Type.GetType(propertyTypeAssemblyQualifiedName), injectedParamerters);
+                }).ToList();
         }
     }
 }
