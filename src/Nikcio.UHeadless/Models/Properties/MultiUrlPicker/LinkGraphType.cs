f698814b6b8a1180cb3adb23715be4061614ba53
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2022-02-04 17:29:48 +0100

    feat: Added MultiUrlPicker model

---
+++ src/Nikcio.UHeadless/Models/Properties/MultiUrlPicker/LinkGraphType.cs
@@ -0,0 +1,20 @@
+﻿using Umbraco.Cms.Core.Models;
+
+namespace Nikcio.UHeadless.Models.Properties.MultiUrlPicker
+{
+    public class LinkGraphType
+    {
+        public string Name { get; set; }
+        public string Target { get; set; }
+        public LinkType Type { get; set; }
+        public string Url { get; set; }
+
+        public LinkGraphType(Link umbracoLink)
+        {
+            Name = umbracoLink.Name;
+            Target = umbracoLink.Target;
+            Type = umbracoLink.Type;
+            Url = umbracoLink.Url;
+        }
+    }
+}
---
+++ src/Nikcio.UHeadless/Models/Properties/MultiUrlPicker/MultiUrlPickerGraphType.cs
@@ -0,0 +1,28 @@
+﻿using Nikcio.UHeadless.Commands.Properties;
+using Nikcio.UHeadless.Models.Dtos.Propreties.PropertyValues;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Cms.Core.Models;
+
+namespace Nikcio.UHeadless.Models.Properties.MultiUrlPicker
+{
+    public class MultiUrlPickerGraphType : PropertyValueBaseGraphType
+    {
+        public List<LinkGraphType> Links { get; set; }
+
+        public MultiUrlPickerGraphType(CreatePropertyValue createPropertyValue) : base(createPropertyValue)
+        {
+            var value = createPropertyValue.Property.GetValue(createPropertyValue.Culture);
+            if(value is IEnumerable<Link> links)
+            {
+                foreach(var link in links)
+                {
+                    Links.Add(new LinkGraphType(link));
+                }
+            }
+        }
+    }
+}
