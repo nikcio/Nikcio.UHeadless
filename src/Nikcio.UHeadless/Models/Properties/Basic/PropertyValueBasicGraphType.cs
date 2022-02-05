430726115b075efa39b588dca6775fccb7342384
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2022-02-05 10:17:19 +0100

    fix: Removed type property

--- src/Nikcio.UHeadless/Models/Properties/Basic/PropertyValueBasicGraphType.cs
+++ src/Nikcio.UHeadless/Models/Properties/Basic/PropertyValueBasicGraphType.cs
@@ -9,12 +9,10 @@ namespace Nikcio.UHeadless.Models.Properties.Basic
     {
         [GraphQLType(typeof(AnyType))]
         public object Value { get; set; }
-        public string Type { get; set; }
 
         public PropertyValueBasicGraphType(CreatePropertyValue createPropertyValue) : base(createPropertyValue)
         {
             Value = createPropertyValue.Property.GetValue(createPropertyValue.Culture);
-            Type = Value?.GetType().AssemblyQualifiedName;
         }
     }
 }
