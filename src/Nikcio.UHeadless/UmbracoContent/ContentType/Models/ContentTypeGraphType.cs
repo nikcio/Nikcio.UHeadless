b0215009a82541389762df56dcb52f637f2a6b30
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2022-02-06 14:57:02 +0100

    docs: Added GraphQL documentation to content type

--- src/Nikcio.UHeadless/UmbracoContent/ContentType/Models/ContentTypeGraphType.cs
+++ src/Nikcio.UHeadless/UmbracoContent/ContentType/Models/ContentTypeGraphType.cs
@@ -2,44 +2,31 @@
 using System.Collections.Generic;
 using Umbraco.Cms.Core.Models.PublishedContent;
 using Umbraco.Cms.Core.Models;
+using HotChocolate;
 
 namespace Nikcio.UHeadless.UmbracoContent.ContentType.Models
 {
     public class ContentTypeGraphType
     {
-        /// <summary>
-        /// Gets the unique key for the content type.
-        /// </summary>
+        [GraphQLDescription("Gets the unique key for the content type.")]
         public Guid Key { get; set; }
 
-        /// <summary>
-        /// Gets the content type identifier.
-        /// </summary>
+        [GraphQLDescription("Gets the content type identifier.")]
         public int Id { get; set; }
 
-        /// <summary>
-        /// Gets the content type alias.
-        /// </summary>
+        [GraphQLDescription("Gets the content type alias.")]
         public string Alias { get; set; }
 
-        /// <summary>
-        /// Gets the content item type.
-        /// </summary>
+        [GraphQLDescription(" Gets the content item type.")]
         public PublishedItemType ItemType { get; set; }
 
-        /// <summary>
-        /// Gets the aliases of the content types participating in the composition.
-        /// </summary>
+        [GraphQLDescription("Gets the aliases of the content types participating in the composition.")]
         public HashSet<string> CompositionAliases { get; set; }
 
-        /// <summary>
-        /// Gets the content variations of the content type.
-        /// </summary>
+        [GraphQLDescription("Gets the content variations of the content type.")]
         public ContentVariation Variations { get; set; }
 
-        /// <summary>
-        /// Gets a value indicating whether this content type is for an element.
-        /// </summary>
+        [GraphQLDescription("Gets a value indicating whether this content type is for an element.")]
         public bool IsElement { get; set; }
     }
 }
