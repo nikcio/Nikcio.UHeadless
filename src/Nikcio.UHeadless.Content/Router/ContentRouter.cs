f4f02119ca04813f72ea63a30d9f070b4b3541d2
Nikcio <nikolajgive@gmail.com>
2022-06-10 22:25:17 +0200

    chore: Clean up

--- src/Nikcio.UHeadless.Content/Router/ContentRouter.cs
+++ src/Nikcio.UHeadless.Content/Router/ContentRouter.cs
@@ -15,29 +15,29 @@ namespace Nikcio.UHeadless.Content.Router {
         /// <summary>
         /// A content redirect repository
         /// </summary>
-        protected readonly IContentRedirectRepository<TContentRedirect> ContentRedirectRepository;
+        protected readonly IContentRedirectRepository<TContentRedirect> contentRedirectRepository;
 
         /// <summary>
         /// A content repository
         /// </summary>
-        protected readonly IContentRepository<TContent, TProperty> ContentRepository;
+        protected readonly IContentRepository<TContent, TProperty> contentRepository;
 
         /// <summary>
         /// The published router
         /// </summary>
-        protected readonly IPublishedRouter PublishedRouter;
+        protected readonly IPublishedRouter publishedRouter;
 
         /// <summary>
         /// The httpcontext accessor
         /// </summary>
-        protected readonly IHttpContextAccessor HttpContextAccessor;
+        protected readonly IHttpContextAccessor httpContextAccessor;
 
         /// <inheritdoc/>
         public ContentRouter(IContentRedirectRepository<TContentRedirect> contentRedirectRepository, IContentRepository<TContent, TProperty> contentRepository, IPublishedRouter publishedRouter, IHttpContextAccessor httpContextAccessor) {
-            ContentRedirectRepository = contentRedirectRepository;
-            ContentRepository = contentRepository;
-            PublishedRouter = publishedRouter;
-            HttpContextAccessor = httpContextAccessor;
+            this.contentRedirectRepository = contentRedirectRepository;
+            this.contentRepository = contentRepository;
+            this.publishedRouter = publishedRouter;
+            this.httpContextAccessor = httpContextAccessor;
         }
 
         /// <inheritdoc/>
@@ -45,8 +45,8 @@ namespace Nikcio.UHeadless.Content.Router {
             if (request.RedirectUrl == null) {
                 return default;
             }
-            var redirect = ContentRedirectRepository.GetContentRedirect(new CreateContentRedirect(request.RedirectUrl, request.IsRedirectPermanent()));
-            var emptyContent = ContentRepository.GetConvertedContent(null, null);
+            var redirect = contentRedirectRepository.GetContentRedirect(new CreateContentRedirect(request.RedirectUrl, request.IsRedirectPermanent()));
+            var emptyContent = contentRepository.GetConvertedContent(null, null);
 
             if (emptyContent == null) {
                 return default;
@@ -62,13 +62,13 @@ namespace Nikcio.UHeadless.Content.Router {
 
         /// <inheritdoc/>
         public virtual async Task<TContent?> GetContentByRouting(string route, string baseUrl, string? culture) {
-            var builder = await PublishedRouter.CreateRequestAsync(new Uri($"{baseUrl}{route}"));
-            var request = await PublishedRouter.RouteRequestAsync(builder, new RouteRequestOptions(RouteDirection.Inbound));
+            var builder = await publishedRouter.CreateRequestAsync(new Uri($"{baseUrl}{route}"));
+            var request = await publishedRouter.RouteRequestAsync(builder, new RouteRequestOptions(RouteDirection.Inbound));
 
             return request.GetRouteResult() switch {
                 UmbracoRouteResult.Redirect => GetRedirect(request),
                 UmbracoRouteResult.NotFound => default,
-                UmbracoRouteResult.Success => request.PublishedContent != null ? ContentRepository.GetConvertedContent(request.PublishedContent, culture) : default,
+                UmbracoRouteResult.Success => request.PublishedContent != null ? contentRepository.GetConvertedContent(request.PublishedContent, culture) : default,
                 _ => default,
             };
         }
@@ -76,14 +76,14 @@ namespace Nikcio.UHeadless.Content.Router {
         /// <inheritdoc/>
         public virtual string SetBaseUrl(string baseUrl) {
             if (string.IsNullOrWhiteSpace(baseUrl)) {
-                if (HttpContextAccessor == null || HttpContextAccessor.HttpContext == null) {
+                if (httpContextAccessor == null || httpContextAccessor.HttpContext == null) {
                     throw new NullReferenceException("HttpContext could not be found");
                 }
 
-                baseUrl = $"{HttpContextAccessor.HttpContext.Request.Scheme}://{HttpContextAccessor.HttpContext.Request.Host.Host}";
+                baseUrl = $"{httpContextAccessor.HttpContext.Request.Scheme}://{httpContextAccessor.HttpContext.Request.Host.Host}";
 
-                if (HttpContextAccessor.HttpContext.Request.Host.Port is not 80 and not 443) {
-                    baseUrl += $":{HttpContextAccessor.HttpContext.Request.Host.Port}";
+                if (httpContextAccessor.HttpContext.Request.Host.Port is not 80 and not 443) {
+                    baseUrl += $":{httpContextAccessor.HttpContext.Request.Host.Port}";
                 }
             }
 
@@ -92,7 +92,7 @@ namespace Nikcio.UHeadless.Content.Router {
 
         /// <inheritdoc/>
         public virtual TContent? GetContentByRouteCache(string route, string? culture, bool preview) {
-            return ContentRepository.GetContent(x => x?.GetByRoute(preview, route, culture: culture), culture);
+            return contentRepository.GetContent(x => x?.GetByRoute(preview, route, culture: culture), culture);
         }
 
     }
