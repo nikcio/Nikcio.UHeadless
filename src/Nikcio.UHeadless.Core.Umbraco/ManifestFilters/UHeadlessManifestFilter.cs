7ebfd9dd3d352f4cb6366505afaba3e7242ff13f
Nikcio <nikolajgive@gmail.com>
2022-07-30 10:48:46 +0200

    feat: Added manifest to composer

---
+++ src/Nikcio.UHeadless.Core.Umbraco/ManifestFilters/UHeadlessManifestFilter.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using Umbraco.Cms.Core.Manifest;
+
+namespace Nikcio.UHeadless.Core.Umbraco.ManifestFilters {
+    internal class UHeadlessManifestFilter : IManifestFilter {
+        public void Filter(List<PackageManifest> manifests) {
+            var assembly = Assembly.GetExecutingAssembly();
+            manifests.Add(new PackageManifest {
+                PackageName = "Nikcío.UHeadless",
+                Version = assembly != null ? FileVersionInfo.GetVersionInfo(assembly.Location).ProductVersion?.ToString() ?? "Unknown" : "Unknown"
+            });
+        }
+    }
+}
