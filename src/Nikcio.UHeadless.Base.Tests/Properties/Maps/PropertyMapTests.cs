c806819195a39108ef121b7b80959ffb735ef966
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2022-08-01 17:00:11 +0200

    test: Added propertymap tests

---
+++ src/Nikcio.UHeadless.Base.Tests/Properties/Maps/PropertyMapTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nikcio.UHeadless.Base.Properties.Commands;
+using Nikcio.UHeadless.Base.Properties.Maps;
+using Nikcio.UHeadless.Base.Properties.Models;
+
+namespace Nikcio.UHeadless.Base.Tests.Properties.Maps {
+    public class PropertyMapTests {
+        public class BasicClass : PropertyValue {
+            public BasicClass(CreatePropertyValue createPropertyValue) : base(createPropertyValue) {
+            }
+        }
+
+        [Test]
+        public void AddEditorMapping_BasicClass() {
+            var propertyMap = new PropertyMap();
+            var editorName = "Editor";
+
+            propertyMap.AddEditorMapping<BasicClass>(editorName);
+            var containsEditor = propertyMap.ContainsEditor(editorName);
+            var types = propertyMap.GetAllTypes();
+
+            Assert.That(containsEditor, Is.True);
+            Assert.Multiple(() => {
+                Assert.That(types.Count(), Is.EqualTo(1));
+                Assert.That(types, Is.InstanceOf<IEnumerable<Type>>());
+            });
+        }
+
+        [Test]
+        public void AddAliasMapping_BasicClass() {
+            var propertyMap = new PropertyMap();
+            var contentTypeAlias = "contentType";
+            var propertyTypeAlias = "propertyType";
+
+            propertyMap.AddAliasMapping<BasicClass>(contentTypeAlias, propertyTypeAlias);
+            var containsEditor = propertyMap.ContainsAlias(contentTypeAlias, propertyTypeAlias);
+            var types = propertyMap.GetAllTypes();
+
+            Assert.That(containsEditor, Is.True);
+            Assert.Multiple(() => {
+                Assert.That(types.Count(), Is.EqualTo(1));
+                Assert.That(types, Is.InstanceOf<IEnumerable<Type>>());
+            });
+        }
+    }
+}
