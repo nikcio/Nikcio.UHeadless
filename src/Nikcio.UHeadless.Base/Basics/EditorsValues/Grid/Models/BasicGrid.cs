dd53e4356e90a2786a0d9eee2fdc8a1e4e7a1ddd
Nikcio <nikolajgive@gmail.com>
2022-10-08 11:42:15 +0200

    chore: Clean up files in PR

--- src/Nikcio.UHeadless.Base/Basics/EditorsValues/Grid/Models/BasicGrid.cs
+++ src/Nikcio.UHeadless.Base/Basics/EditorsValues/Grid/Models/BasicGrid.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using HotChocolate;
+﻿using HotChocolate;
 using Nikcio.UHeadless.Base.Properties.Commands;
 using Nikcio.UHeadless.Base.Properties.Models;
-using Umbraco.Cms.Core.Strings;
 
 namespace Nikcio.UHeadless.Base.Basics.EditorsValues.Grid.Models {
-    
+
+    /// <summary>
+    /// Represents a grid edtior
+    /// </summary>
     [GraphQLDescription("Represents a grid editor.")]
     public class BasicGrid : PropertyValue {
         /// <summary>
