9d74aa19bc2fdaa0507433a7574719ceac17756c
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2023-04-12 13:12:42 +1000

    test: Added Property editor test

---
+++ src/Nikcio.UHeadless.IntegrationTests/Content/Queries/ContentAtRootTests.cs
@@ -0,0 +1,259 @@
+using Nikcio.UHeadless.IntegrationTests.Extensions;
+using StrawberryShake;
+
+namespace Nikcio.UHeadless.IntegrationTests.Content.Queries;
+
+[TestFixture]
+public class ContentAtRootTests : IntegrationTestBase
+{
+   
+    [Test]
+    public async Task GeneralGetContentAtRoot_Test()
+    {
+        var setup = new Setup();
+
+        var result = await setup.UHeadlessClient.GeneralGetContentAtRoot.ExecuteAsync();
+
+        result.Errors.EnsureNoErrors();
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.Data, Is.Not.Null);
+        Assert.That(result.Data!.ContentAtRoot, Is.Not.Null);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes, Is.Not.Null);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.Count(), Is.GreaterThan(0));
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node != null), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.Id > 0), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.Key != null), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => !string.IsNullOrEmpty(node!.Name)), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.CreatorId == -1), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.WriterId == -1), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.Properties != null && node.Properties.All(property => property != null && !string.IsNullOrEmpty(property.Alias))), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => !string.IsNullOrEmpty(node!.ItemType.ToString())), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.Level > 0), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.Parent == null), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.Redirect == null || !string.IsNullOrEmpty(node.Redirect.RedirectUrl)), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.SortOrder > -1), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.TemplateId == null || node!.TemplateId > 0), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => !string.IsNullOrEmpty(node!.Url)), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => !string.IsNullOrEmpty(node!.UrlSegment)), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => !string.IsNullOrEmpty(node!.AbsoluteUrl)), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.Children == null || node.Children.All(child => !string.IsNullOrEmpty(child!.Name))), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.Children == null || node.Children.All(child => child!.CreatorId == -1)), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.Children == null || node.Children.All(child => child!.WriterId == -1)), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.Children == null || node.Children.All(child => child!.Properties != null && child.Properties.All(property => !string.IsNullOrEmpty(property!.Alias)))), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.Children == null || node.Children.All(child => !string.IsNullOrEmpty(child!.ItemType.ToString()))), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.Children == null || node.Children.All(child => child!.Level > node.Level)), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.Children == null || node.Children.All(child => child!.Parent != null)), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.Children == null || node.Children.All(child => !string.IsNullOrEmpty(child!.Parent!.Name))), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.Children == null || node.Children.All(child => child!.Redirect == null || !string.IsNullOrEmpty(child.Redirect.RedirectUrl))), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.Children == null || node.Children.All(child => child!.SortOrder > -1)), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.Children == null || node.Children.All(child => child!.TemplateId == null || child!.TemplateId > 0)), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.Children == null || node.Children.All(child => !string.IsNullOrEmpty(child!.Url))), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.Children == null || node.Children.All(child => !string.IsNullOrEmpty(child!.UrlSegment))), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.Children == null || node.Children.All(child => !string.IsNullOrEmpty(child!.AbsoluteUrl))), Is.True);
+    }
+
+    [Test]
+    public async Task GetNodeIdGetContentAtRoot_Test()
+    {
+        var setup = new Setup();
+
+        var result = await setup.UHeadlessClient.GetNodeIdGetContentAtRoot.ExecuteAsync();
+
+        result.Errors.EnsureNoErrors();
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.Data, Is.Not.Null);
+        Assert.That(result.Data!.ContentAtRoot, Is.Not.Null);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes, Is.Not.Null);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.Count(), Is.GreaterThan(0));
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node != null), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.Id > 0), Is.True);
+    }
+
+    [Test]
+    public async Task GetPropertiesContentAtRoot_Test()
+    {
+        var setup = new Setup();
+
+        var result = await setup.UHeadlessClient.GetPropertiesContentAtRoot.ExecuteAsync();
+
+        result.Errors.EnsureNoErrors();
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.Data, Is.Not.Null);
+        Assert.That(result.Data!.ContentAtRoot, Is.Not.Null);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes, Is.Not.Null);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.Count(), Is.GreaterThan(0));
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node != null), Is.True);
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.Properties != null));
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.Properties!.All(property => !string.IsNullOrEmpty(property!.Alias))));
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.Properties!.All(property => !string.IsNullOrEmpty(property!.EditorAlias))));
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.Properties!.All(property => property!.Value != null)));
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.Properties!.All(property => IsPropertyValueValid(property!.Value!))));
+        Assert.That(result.Data!.ContentAtRoot!.Nodes!.All(node => node!.Children == null || node.Children.All(child => child!.Properties != null && child.Properties.All(property => IsPropertyValueValid(property!.Value!)))));
+    }
+
+    public bool IsPropertyValueValid(IGetPropertiesContentAtRoot_ContentAtRoot_Nodes_Properties_Value value)
+    {
+        if(value is IGetPropertiesContentAtRoot_ContentAtRoot_Nodes_Properties_Value_BasicBlockListModel blockListModel)
+        {
+            Assert.That(blockListModel.Blocks, Is.Not.Null);
+            Assert.That(blockListModel.Blocks!.All(block => block.ContentProperties != null));
+            Assert.That(blockListModel.Blocks!.All(block => block.ContentProperties.All(property => property!.Value != null)));
+            // TODO: ContentProperty validation
+            // TODO: SettingProperty validation
+            return true;
+        }
+        else if(value is IGetPropertiesContentAtRoot_ContentAtRoot_Nodes_Properties_Value_BasicContentPicker contentPicker)
+        {
+            Assert.That(contentPicker.Alias, Is.Not.Null);
+            Assert.That(contentPicker.Alias, Is.Not.Empty);
+            Assert.That(contentPicker.ContentList, Is.Not.Null);
+            Assert.That(contentPicker.ContentList!.All(item => item.Id > 0), Is.True);
+            return true;
+        }
+        else if(value is IGetPropertiesContentAtRoot_ContentAtRoot_Nodes_Properties_Value_BasicDateTimePicker dateTimePicker)
+        {
+            Assert.That(dateTimePicker.Alias, Is.Not.Null);
+            Assert.That(dateTimePicker.Alias, Is.Not.Empty);
+            Assert.That(dateTimePicker.DateTime == null || dateTimePicker.DateTime != default, Is.True);
+
+            return true;
+        }
+        else if(value is IGetPropertiesContentAtRoot_ContentAtRoot_Nodes_Properties_Value_BasicLabel label)
+        {
+            Assert.That(label.Label, Is.Not.Null);
+            Assert.That(label.Label, Is.Not.Empty);
+            return true;
+        }
+        else if(value is IGetPropertiesContentAtRoot_ContentAtRoot_Nodes_Properties_Value_BasicMediaPicker mediaPicker)
+        {
+            Assert.That(mediaPicker.Alias, Is.Not.Null);
+            Assert.That(mediaPicker.Alias, Is.Not.Empty);
+            Assert.That(mediaPicker.MediaItems, Is.Not.Null);
+            Assert.That(mediaPicker.MediaItems.All(item => item.Id > 0));
+            Assert.That(mediaPicker.MediaItems.All(item => !string.IsNullOrEmpty(item.Url)));
+            return true;
+        }
+        else if(value is IGetPropertiesContentAtRoot_ContentAtRoot_Nodes_Properties_Value_BasicMemberPicker memberPicker)
+        {
+            Assert.That(memberPicker.Alias, Is.Not.Null);
+            Assert.That(memberPicker.Alias, Is.Not.Empty);
+            Assert.That(memberPicker.Members, Is.Not.Null);
+            Assert.That(memberPicker.Members.All(member => member.Id > 0));
+            Assert.That(memberPicker.Members.All(member => !string.IsNullOrEmpty(member.Name)));
+            Assert.That(memberPicker.Members.All(member => member.Properties != null));
+            // TODO: Member property validation
+            return true;
+        }
+        else if(value is IGetPropertiesContentAtRoot_ContentAtRoot_Nodes_Properties_Value_BasicMultiUrlPicker multiUrlPicker)
+        {
+            Assert.That(multiUrlPicker.Alias, Is.Not.Null);
+            Assert.That(multiUrlPicker.Alias, Is.Not.Empty);
+            Assert.That(multiUrlPicker.Links, Is.Not.Null);
+            Assert.That(multiUrlPicker.Links.All(link => !string.IsNullOrEmpty(link.Name)));
+            Assert.That(multiUrlPicker.Links.All(link => !string.IsNullOrEmpty(link.Url)));
+            return true;
+        }
+        else if(value is IGetPropertiesContentAtRoot_ContentAtRoot_Nodes_Properties_Value_BasicPropertyValue basicValue)
+        {
+            return true;
+        }
+        else if(value is IGetPropertiesContentAtRoot_ContentAtRoot_Nodes_Properties_Value_BasicRichText richText)
+        {
+            Assert.That(richText.Alias, Is.Not.Null);
+            Assert.That(richText.Alias, Is.Not.Empty);
+            Assert.That(richText.RichText, Is.Not.Null);
+            Assert.That(richText.SourceValue == null || !string.IsNullOrEmpty(richText.RichText), Is.True);            return true;
+        }
+        else if(value is IGetPropertiesContentAtRoot_ContentAtRoot_Nodes_Properties_Value_BasicUnsupportedPropertyValue unsupportedValue)
+        {
+            Assert.That(unsupportedValue.Message, Is.Not.Null);
+            Assert.That(unsupportedValue.Message, Is.Not.Empty);
+            return true;
+        }
+
+        return false;
+    }
+    
+    public bool IsPropertyValueValid(IGetPropertiesContentAtRoot_ContentAtRoot_Nodes_Children_Properties_Value value)
+    {
+        if(value is IGetPropertiesContentAtRoot_ContentAtRoot_Nodes_Children_Properties_Value_BasicBlockListModel blockListModel)
+        {
+            Assert.That(blockListModel.Blocks, Is.Not.Null);
+            Assert.That(blockListModel.Blocks!.All(block => block.ContentProperties != null));
+            Assert.That(blockListModel.Blocks!.All(block => block.ContentProperties.All(property => property!.Value != null)));
+            // TODO: ContentProperty validation
+            // TODO: SettingProperty validation
+            return true;
+        }
+        else if(value is IGetPropertiesContentAtRoot_ContentAtRoot_Nodes_Children_Properties_Value_BasicContentPicker contentPicker)
+        {
+            Assert.That(contentPicker.Alias, Is.Not.Null);
+            Assert.That(contentPicker.Alias, Is.Not.Empty);
+            Assert.That(contentPicker.ContentList, Is.Not.Null);
+            Assert.That(contentPicker.ContentList!.All(item => item.Id > 0), Is.True);
+            return true;
+        }
+        else if(value is IGetPropertiesContentAtRoot_ContentAtRoot_Nodes_Children_Properties_Value_BasicDateTimePicker dateTimePicker)
+        {
+            Assert.That(dateTimePicker.Alias, Is.Not.Null);
+            Assert.That(dateTimePicker.Alias, Is.Not.Empty);
+            Assert.That(dateTimePicker.DateTime == null || dateTimePicker.DateTime != default, Is.True);
+            return true;
+        }
+        else if(value is IGetPropertiesContentAtRoot_ContentAtRoot_Nodes_Children_Properties_Value_BasicLabel label)
+        {
+            Assert.That(label.Label, Is.Not.Null);
+            Assert.That(label.Label, Is.Not.Empty);
+            return true;
+        }
+        else if(value is IGetPropertiesContentAtRoot_ContentAtRoot_Nodes_Children_Properties_Value_BasicMediaPicker mediaPicker)
+        {
+            Assert.That(mediaPicker.Alias, Is.Not.Null);
+            Assert.That(mediaPicker.Alias, Is.Not.Empty);
+            Assert.That(mediaPicker.MediaItems, Is.Not.Null);
+            Assert.That(mediaPicker.MediaItems.All(item => item.Id > 0));
+            Assert.That(mediaPicker.MediaItems.All(item => !string.IsNullOrEmpty(item.Url)));
+            return true;
+        }
+        else if(value is IGetPropertiesContentAtRoot_ContentAtRoot_Nodes_Children_Properties_Value_BasicMemberPicker memberPicker)
+        {
+            Assert.That(memberPicker.Alias, Is.Not.Null);
+            Assert.That(memberPicker.Alias, Is.Not.Empty);
+            Assert.That(memberPicker.Members, Is.Not.Null);
+            Assert.That(memberPicker.Members.All(member => member.Id > 0));
+            Assert.That(memberPicker.Members.All(member => !string.IsNullOrEmpty(member.Name)));
+            Assert.That(memberPicker.Members.All(member => member.Properties != null));
+            // TODO: Member property validation
+            return true;
+        }
+        else if(value is IGetPropertiesContentAtRoot_ContentAtRoot_Nodes_Children_Properties_Value_BasicMultiUrlPicker multiUrlPicker)
+        {
+            Assert.That(multiUrlPicker.Alias, Is.Not.Null);
+            Assert.That(multiUrlPicker.Alias, Is.Not.Empty);
+            Assert.That(multiUrlPicker.Links, Is.Not.Null);
+            Assert.That(multiUrlPicker.Links.All(link => !string.IsNullOrEmpty(link.Name)));
+            Assert.That(multiUrlPicker.Links.All(link => !string.IsNullOrEmpty(link.Url)));
+            return true;
+        }
+        else if(value is IGetPropertiesContentAtRoot_ContentAtRoot_Nodes_Children_Properties_Value_BasicPropertyValue basicValue)
+        {
+            return true;
+        }
+        else if(value is IGetPropertiesContentAtRoot_ContentAtRoot_Nodes_Children_Properties_Value_BasicRichText richText)
+        {
+            Assert.That(richText.Alias, Is.Not.Null);
+            Assert.That(richText.Alias, Is.Not.Empty);
+            Assert.That(richText.RichText, Is.Not.Null);
+            Assert.That(richText.SourceValue == null || !string.IsNullOrEmpty(richText.RichText), Is.True);
+            return true;
+        }
+        else if(value is IGetPropertiesContentAtRoot_ContentAtRoot_Nodes_Children_Properties_Value_BasicUnsupportedPropertyValue unsupportedValue)
+        {
+            Assert.That(unsupportedValue.Message, Is.Not.Null);
+            Assert.That(unsupportedValue.Message, Is.Not.Empty);
+            return true;
+        }
+
+        return false;
+    }
+}
