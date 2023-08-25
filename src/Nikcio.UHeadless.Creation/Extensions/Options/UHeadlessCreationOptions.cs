3120fe64446fce2c0fef43d676662668dcb36bd1
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2023-08-25 08:06:10 +0200

    feat: Creation extensions

---
+++ src/Nikcio.UHeadless.Creation/Extensions/Options/UHeadlessCreationOptions.cs
@@ -0,0 +1,14 @@
+﻿using Nikcio.UHeadless.Base.Properties.Extensions;
+
+namespace Nikcio.UHeadless.Creation.Extensions.Options;
+
+/// <summary>
+/// Options for UHeadless
+/// </summary>
+public class UHeadlessCreationOptions
+{
+    /// <summary>
+    /// Options for the property services
+    /// </summary>
+    public virtual PropertyServicesOptions PropertyServicesOptions { get; set; } = new();
+}
---
+++ src/Nikcio.UHeadless.Creation/Extensions/UHeadlessExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Nikcio.UHeadless.Base.Properties.Extensions;
+using Nikcio.UHeadless.ContentTypes.Extensions;
+using Nikcio.UHeadless.Core.Reflection.Extensions;
+using Nikcio.UHeadless.Creation.Extensions.Options;
+using Umbraco.Cms.Core.DependencyInjection;
+
+namespace Nikcio.UHeadless.Creation.Extensions;
+
+/// <summary>
+/// The default UHeadless extensions used for the default setup
+/// </summary>
+public static class UHeadlessExtensions
+{
+    /// <summary>
+    /// Adds all services the UHeadless package needs
+    /// </summary>
+    /// <param name="builder">The Umbraco builder</param>
+    /// <returns></returns>
+    public static IUmbracoBuilder AddUHeadless(this IUmbracoBuilder builder)
+    {
+        var uHeadlessCreationOptions = new UHeadlessCreationOptions();
+        return AddUHeadless(builder, uHeadlessCreationOptions);
+    }
+
+    /// <summary>
+    /// Adds all services the UHeadless package needs
+    /// </summary>
+    /// <param name="builder">The Umbraco builder</param>
+    /// <param name="uHeadlessCreationOptions"></param>
+    /// <returns></returns>
+    public static IUmbracoBuilder AddUHeadless(this IUmbracoBuilder builder, UHeadlessCreationOptions uHeadlessCreationOptions)
+    {
+        builder.Services
+            .AddReflectionServices()
+            .AddPropertyServices(uHeadlessCreationOptions.PropertyServicesOptions)
+            .AddContentTypeServices();
+
+        return builder;
+    }
+}
